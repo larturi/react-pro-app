3870ac179c643f8bf7669c1c9301a9f30cdb5a47
lea.arturi <lea.arturi@gmail.com>
2021-12-12 14:35:19 -0300

    Rutas dinamicas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
 
 import logo from '../logo.svg';
+import { routes } from './routes';
 
 export const Navigation = () => {
     return (
@@ -11,24 +12,33 @@ export const Navigation = () => {
                     <img src={logo} alt="React Logo" />
 
                     <ul>
-                        <li>
-                            <NavLink to="/home" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/about" className={ ({isActive}) => isActive ? 'nav-active' : '' }>About</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/users" className={ ({isActive}) => isActive ? 'nav-active' : '' }>Users</NavLink>
-                        </li>
+                       {
+                            routes.map(route => (
+                                 <li key={route.to}>
+                                    <NavLink 
+                                        to={route.path} 
+                                        className={ ({isActive}) => isActive ? 'nav-active' : '' }
+                                    >
+                                        {route.name}
+                                    </NavLink>
+                                </li>
+                            ))
+                       }
                     </ul>
                 </nav>
 
                 <Routes>
-                    <Route path="about" element={ <h1>About Page</h1> } />
-                    <Route path="users" element={ <h1>Users Page</h1> } />
-                    <Route path="home" element={ <h1>Home Page</h1> } />
-
-                    <Route path="/*" element={ <Navigate to="home" replace /> } />
+                    {
+                        routes.map(route => (
+                            <Route 
+                                key={route.to}
+                                path={route.path} 
+                                element={<route.Component />} 
+                            />
+                        ))
+                    }
+                    
+                    <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
                 </Routes>
             </div>
         </BrowserRouter>
---
+++ src/routes/routes.ts
@@ -0,0 +1,30 @@
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+interface Route {
+    to: string;
+    path: string;
+    name: string;
+    Component: () => JSX.Element;
+}
+
+export const routes: Route[] = [
+    {
+        to: '/lazy1',
+        path: 'lazy1',
+        Component: LazyPage1,
+        name: 'LazyPage1',
+    },
+    {
+        to: '/lazy2',
+        path: 'lazy2',
+        Component: LazyPage2,
+        name: 'LazyPage2',
+    },
+    {
+        to: '/lazy3',
+        path: 'lazy3',
+        Component: LazyPage3,
+        name: 'LazyPage3',
+    },
+
+]
