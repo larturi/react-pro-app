2ba275cfce1d0940b1839f935285f7b6943ded4b
Leandro Arturi <lea.arturi@gmail.com>
2021-12-18 10:59:10 -0300

    Component Compound Listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductCard } from '../components/ProductCard';
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 
 const product = {
     id: '1',
@@ -20,8 +20,14 @@ export const ShoppingPage = () => {
 
                 <ProductCard product={product}>
                     <ProductCard.Image />  
-                    <ProductCard.Title />
+                    <ProductCard.Title title={'Hola'}/>
                     <ProductCard.Buttons />
+                </ProductCard>
+
+                <ProductCard product={product}>
+                    <ProductImage />  
+                    <ProductTitle/>
+                    <ProductButtons />
                 </ProductCard>
             </div>
         </div>
